693b19e433cb53b99b8fb5a76a28836f3e3b1b83
shajin <shajin.sha10@gmail.com>
2021-11-20 04:30:15 +0530

    image url

--- Bionic/main.py
+++ Bionic/main.py
@@ -6,9 +6,9 @@ def RunApp():
     Body(
         Text("Craft Beautiful, Fast User Experience", classname='Welcome-text'),
         Text('| Bionic is Python Framework for crafting beautiful, fast user experiences for web and is free and open source', classname='info-text'),
-        Img('./src/iMac.png', classname='iMac'),
-        Img('./src/iPhone.png', classname='iPhone'),
-        Img('./src/iPad.png', classname='iPad'),
-        Img('./src/Processor.png', classname='Processor'),
+        Img('https://i.ibb.co/jhkDSGF/iMac.png', classname='iMac'),
+        Img('https://i.ibb.co/qgnq7BZ/iPhone.png', classname='iPhone'),
+        Img('https://i.ibb.co/mF3bYcz/iPad.png', classname='iPad'),
+        Img('https://i.ibb.co/tYLnTdb/Processor.png', classname='Processor'),
 
     ),
