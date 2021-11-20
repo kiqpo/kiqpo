05ee437b9909e968aaccf93234d581196557e0c5
Tarunav BA <tarunavba@gmail.com>
2021-11-20 11:12:54 +0530

    Update main.py
    Added a couple of more words so that people can understand Bionic even well.

--- Bionic/main.py
+++ Bionic/main.py
@@ -5,7 +5,7 @@ def RunApp():
     Head(title="Welcome"),
     Body(
         BottomNav(),
-        Text("Hello"),
+        Text("Hello, there! 👋. Thanks for checking out Bionic web framework amogst all other frameworks. To get started checkout the bionic documentation."),
         Button(text="btn", classname='btn'),
 
     ),
