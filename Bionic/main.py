ca32e4387a0f2d361408715422e176ccf6425928
Tarunav BA <tarunavba@gmail.com>
2021-11-20 11:15:47 +0530

    Update main.py
    Add link to documentation

--- Bionic/main.py
+++ Bionic/main.py
@@ -5,7 +5,7 @@ def RunApp():
     Head(title="Welcome"),
     Body(
         BottomNav(),
-        Text("Hello, there! 👋. Thanks for checking out Bionic web framework amogst all other frameworks. To get started checkout the bionic documentation."),
+        Text("Hello, there! 👋. Thanks for checking out Bionic web framework amogst all other frameworks. To get started checkout the bionic documentation at https://bionic-py.github.io/Bionic-Documentation/."),
         Button(text="btn", classname='btn'),
 
     ),
