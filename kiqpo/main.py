19ae7aba79abe3ea093a6c3b1591a16d10246e81
shajin-sha <shajin.sha10@gmail.com>
2022-01-27 16:26:43 +0530

    commets added

--- kiqpo/main.py
+++ kiqpo/main.py
@@ -2,19 +2,25 @@ from view import *
 
 
 def RunApp():
+    # head tag
     Head(title="Kiqpo", themeColor="#ffffff"),
     Body(
+        # navbar at the top.
         TopNav(backgroundColor="#ffffff", labelgap="3%", label=Text(
             "Kiqpo", TextStyle=TextStyle(
                 Color='green',
                 FontSize='220%',
                 Weight='900'),
         )),
+
+        # text with padding
         Padding(child=Text('Hello from kiqpo.', TextStyle=TextStyle(
             FontSize="160%",
             Color="#292827",
             Weight='900'
         )), top='0.5%', left="1%"),
+
+        # text with padding & sub heading
         Padding(child=Text('Runing in kiqpo in', Id="platform", TextStyle=TextStyle(
             FontSize='85%',
             Weight="550"
