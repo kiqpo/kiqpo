617c2a13fa445ae3535d5347bd9e7cf50a4e4e6f
shajin-sha <shajin.sha10@gmail.com>
2022-03-04 19:29:13 +0530

    git ignore added

--- kiqpo/pages/account.py
+++
@@ -1,9 +0,0 @@
-from view import *
-
-
-def account():
-    return Div(Padding(
-        Flex(
-            Text("Account settings", TextStyle=TextStyle(FontSize=Screen(290), Weight='900')),
-            Text("shajin_sha", TextStyle=TextStyle(Weight='500'))),
-    ),classname="Account")
--- kiqpo/pages/home.py
+++
@@ -1,9 +0,0 @@
-from view import *
-
-
-def home():
-    return Div(Padding(
-        Flex(
-            Text("Welcome", TextStyle=TextStyle(FontSize=Screen(290), Weight='900')),
-            Text("shajin_sha", TextStyle=TextStyle(Weight='500'))),
-    ),classname="Home")
